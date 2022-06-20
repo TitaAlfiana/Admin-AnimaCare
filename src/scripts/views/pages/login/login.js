56a5a138c676be17ddbc852758d83399b16ea715
todayisvin <kevinpratamaoke@gmail.com>
2022-06-20 09:41:51 +0700

    update tampilan

--- src/scripts/views/pages/login/login.js
+++ src/scripts/views/pages/login/login.js
@@ -1,23 +1,22 @@
 import './login.css';
-import Logo from '../../../../public/icons/72x72.png';
+import Logo from '../../../../public/Logo.png';
 
 const Login = {
   async render() {
     return `
     <form class="form-signin">
         <img class="mb-4" src="{${Logo}}" alt="" width="72" height="72">
-        <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-        <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-        <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-        <div class="checkbox mb-3">
-            <label>
-            <input type="checkbox" value="remember-me"> Remember me
-            </label>
+        <h1 class="h3 mb-3 font-weight-normal judul">Selamat Datang!</h1>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Alamat Email</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
         </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-        <p class="mt-5 mb-3 text-muted">&copy; AnimaCare 2022</p>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Kata Sandi</label>
+          <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <button class="btn btn-lg btn-primary btn-block btn-submit" type="submit">Masuk</button>
+        <p class="mt-5 mb-3 copyright">&copy; AnimaCare 2022</p>
     </form>
     `;
   },
